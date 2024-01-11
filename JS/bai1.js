0c0a3e1fac8d348e99099d8c8f16742360f0bd7b
1petercuong <155179049+1petercuong@users.noreply.github.com>
2024-01-11 10:27:09 +0700

    Add files via upload

---
+++ JS/bai1.js
@@ -0,0 +1,9 @@
+function ktra(a){
+    
+if (a &1){
+    alert("a là số chẵn");
+}
+else{
+    alert("a là số lẽ");
+}
+}
---
+++ JS/bai3.js
@@ -0,0 +1,7 @@
+function ktra(x){
+    if(x==a)
+    alert("YES")
+    else
+    alert("NO")
+    
+}
